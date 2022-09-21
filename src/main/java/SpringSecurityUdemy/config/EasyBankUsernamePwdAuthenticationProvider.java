d3c5c81ab1d037bbd1c57a0e1532db5fd7d15231
michal-jamula <michal5832@gmail.com>
2022-09-21 22:15:56 +0100

    Finished section 5: Understanding authentication provider and implementing it

---
+++ src/main/java/SpringSecurityUdemy/config/EasyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,56 @@
+package SpringSecurityUdemy.config;
+
+import SpringSecurityUdemy.model.Customer;
+import SpringSecurityUdemy.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.support.CustomSQLErrorCodesTranslation;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class EasyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        Optional<Customer> optionalCustomer = customerRepository.findByEmail(username);
+        if (optionalCustomer.isPresent()) {
+            Customer customer = optionalCustomer.get();
+            if (passwordEncoder.matches(pwd, customer.getPwd())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customer.getRole()));
+                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+            } else {
+                throw new BadCredentialsException("Invalid password!");
+            }
+        }else {
+            throw new BadCredentialsException("No user registered with this details!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authenticationType) {
+        return authenticationType.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
